62a5be9615761e111e288a531891586f1dfb24da
Denio1212 <shehuden22@yahoo.com>
2022-09-29 13:59:40 -0700

    Prettiee Putties

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -4,7 +4,7 @@
 * main - prints the text given
 * Return should answer 0 if exited properly
  */
-int main(void)
+int main (void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);

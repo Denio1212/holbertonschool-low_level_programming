019971959888aa381361b4e4bd6cc80ce4d25646
Denio1212 <shehuden22@yahoo.com>
2022-09-29 13:36:29 -0700

    Prettiee Putties

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/**
+ * main --- prints the phase we are writing
+ * return needs to be zero to exit properly,,, id not zero there are errors
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+		return(0);
+
+}	

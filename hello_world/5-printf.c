0ff68ae6497710c0733d177987b8d3ccdf790259
Denio1212 <shehuden22@yahoo.com>
2022-09-29 14:10:10 -0700

    If only i wasn't dislexic

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/** main prints the txt
+* Return gives 0 when everything is aok
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}

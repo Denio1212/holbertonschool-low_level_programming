457db324cb5d87a8d6095c7cd1744dcdd7b40ec0
Denio1212 <shehuden22@yahoo.com>
2022-09-29 14:31:14 -0700

    Thats full of

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+printf("Size of int: %zu bytes(s)\n", sizeof(char));
+printf("Size of float: %zu bytes(s)\n", sizeof(int));
+printf("Size of double: %zu bytes\(s)n", sizeof(long int));
+printf("Size of char: %zu bytes\(s)n", sizeof(float));
+printf("Size of char: %zu bytes\(s)n", sizeof(long long int));
+return (0);
+}

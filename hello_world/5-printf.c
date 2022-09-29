af2092079d09c851d5311f76aca0b9195515120d
Denio1212 <shehuden22@yahoo.com>
2022-09-29 14:13:48 -0700

    If only i wasn't dislexic

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main prints the txt
-* Return gives 0 when everything is aok
+* main prints the txt
+* Return: 0 if exited properly, non-zero otherwise
 */
 
 int main(void)

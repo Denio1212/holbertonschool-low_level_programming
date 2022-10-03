3c37e568b982803eb42c6d9e6a0d58476a7c1d24
Denio1212 <shehuden22@yahoo.com>
2022-10-03 13:51:01 -0700

    So many numbers man

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers
+ * starting from 0 with putchar
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	int b;
+
+	for (b = 0; b < 10; b++)
+	{
+		putchar(b);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}

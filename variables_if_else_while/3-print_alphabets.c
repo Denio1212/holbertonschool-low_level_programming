fa72820d4f2a969afafdb2bfa58868a78068160b
Denio1212 <shehuden22@yahoo.com>
2022-10-03 12:57:45 -0700

    Stonks

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase,
+ * and then in uppercase, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int cc;
+
+	for (cc = 'a'; cc <= 'z'; cc++)
+		putchar(cc);
+
+	for (cc = 'A'; cc <= 'Z'; cc++)
+		putchar(cc);
+
+	return (0);
+}

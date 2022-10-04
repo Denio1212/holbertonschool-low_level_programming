ea11f37530f08e1706aafbd537f38f4f95b8d1d8
Denio1212 <shehuden22@yahoo.com>
2022-10-04 04:15:25 -0700

    sdrawkcab

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the lowercase alphabet in reverse,
- * followed by a new line
+ * main - prints the lowercase alphabet in reverse
  * Return: Always 0 (Success)
 */
 
@@ -18,9 +17,3 @@ int main(void)
 
 	return (0);
 }
-/**
- * Instead of raising r++ i lowered the value of it by using
- * r-- which lowers it by one
- * Otherwise it is very similar to the regular alphabet
- * program
- */

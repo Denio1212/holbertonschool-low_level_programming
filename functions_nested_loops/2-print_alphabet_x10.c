f66b356ab704e42d059ec84a073ce66e2895cdff
Denio1212 <shehuden22@yahoo.com>
2022-10-04 07:37:22 -0700

    Multiplier

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+	char l;
+	int o;
+
+	o = 0;
+
+	while (o < 10)
+	{
+		l = 'a';
+		while (l <= 'z')
+		{
+			_putchar(l);
+			l++;
+		}
+		_putchar('\n');
+			o++;
+	}
+
+}
+
+

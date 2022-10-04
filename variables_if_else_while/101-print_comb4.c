b15e0848bd6b0171db297d116357537394ccc620
Denio1212 <shehuden22@yahoo.com>
2022-10-04 04:20:32 -0700

    Triplets

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -32,13 +32,3 @@ int main(void)
 	putchar('\n');
 		return (0);
 }
-/**
- * The a variable starts at 0 and finishes at 9
- * the variable b starts from 1 and finishes at 9
- * the variable c starts from 2 and finishes at 9
- * this prevents the same type of number like 012 or 210 or 201
- * the a and b digits are never allowed to be over 4 and 7 more than once
- * this makes the combinations be as small as possible but also including all
- * numbers
- */
-

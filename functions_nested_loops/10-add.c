cef2878c4f869ccdf36bcb1e79d6735e091d29fc
Denio1212 <shehuden22@yahoo.com>
2022-10-05 00:29:05 -0700

    An addition doesnt always make you stronger

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @l: int to be added to r
+ * @r: int to be added to l
+ * Return: sum of l and r
+ */
+int add(int l, int r)
+{
+	int e;
+
+	e = l + r;
+
+	return (e);
+}
+

b0c82f5e30cf023738266302aec7463e03cc7c78
Denio1212 <shehuden22@yahoo.com>
2022-10-06 01:41:03 -0700

    Multiply this add that, man its rough around here!

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - multiplies @a and @b
+ * @a: the int to be multiplied to @b
+ * @b: the int to be multiplied to @a
+ * Return: Returns the result
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+
+	return (mul);
+}

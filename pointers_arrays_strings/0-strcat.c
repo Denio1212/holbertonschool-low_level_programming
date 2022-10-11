5e1439ce24204c7412a74874751445180885c23c
Denio1212 <shehuden22@yahoo.com>
2022-10-11 13:18:45 -0700

    Link to my bio

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strcat - links two strings together
+ * @dest: The destination string
+ * @src: the string being changed
+ * Return: Returns the src dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}

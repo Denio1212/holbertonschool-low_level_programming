c064bd282d5fc7d8e474b5af63f84a85caf77569
Denio1212 <shehuden22@yahoo.com>
2022-10-10 23:47:43 -0700

    Gnorw lla saw ti tub, rach eht nitup rimidalv

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - prints the selected string in reverse
+ * @s: The string
+ */
+
+void print_rev(char *s)
+{
+	int i, j, count;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	count = i;
+
+	for (j = count; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+
+	_putchar('\n');
+}

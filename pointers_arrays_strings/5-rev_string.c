2e3bfd21cd25a0215415b6daef302cee524d64bd
Denio1212 <shehuden22@yahoo.com>
2022-10-11 00:07:39 -0700

    Reversal

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,8 @@
 #include "main.h"
-
+/**
+ * rev_string  - reverses any given string
+ * @s: the string reversed
+ */
 void rev_string(char *s)
 {
 	char t;
@@ -8,13 +11,13 @@ void rev_string(char *s)
 	size = 0;
 	sizer = 0;
 
-	while(s[size] != '\0')
+	while (s[size] != '\0')
 	{
 		size++;
 	}
 	sizer = size - 1;
 
-	for(i = 0; i < size / 2; i++)
+	for (i = 0; i < size / 2; i++)
 	{
 		t = s[i];
 		s[i] = s[sizer];

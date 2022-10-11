593f6b86b54943f63fa28f43722171947ddb01af
Denio1212 <shehuden22@yahoo.com>
2022-10-11 01:03:20 -0700

    Multiple stringers

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints an array of strings
+ * @n: input string
+ * @a: array
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+
+	{
+		if (i == 0)
+			printf("%d", a[i]);
+		else
+			printf(", %d", a[i]);
+	}
+	printf("\n");
+}

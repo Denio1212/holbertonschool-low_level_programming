5a769c7c3f15dae204467e24d9c22d0e9e2295cd
Denio1212 <shehuden22@yahoo.com>
2022-10-13 00:54:30 -0700

    No cap

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,28 +1,28 @@
 #include "main.h"
+#include <stdio.h>
 
 
-
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
 	int i, j;
 
-	char spr[13] = {' ', '\t', '\n', ';', '.', '!', '?', '"', '(', ')', 			'{', '}'};
+	char spr[13] = {' ', '\t', '\n', ',', ';', ',', '!', '?', '"', '(', 			')','{', '}'};
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
-			str[i] -= 32;
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
 
-		for (j = 0; j < 13; i++)
+		for (j = 0; j <= 12; j++)
 		{
-			if (str[i] == spr[j])
+			if (s[i] == spr[j])
 			{
-				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-					str[i + 1] -= 32;
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+					s[i + 1] -= 32;
 				
 			}	
 		}
 	}
-	return (str);
+	return (s);
 }
 					

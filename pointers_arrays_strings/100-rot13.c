55efbb1f963fad06ab70752fd46892d59a4ff6ef
Denio1212 <shehuden22@yahoo.com>
2022-10-14 12:18:18 -0700

    Friday the 13-th

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *rot13 - encrypts a string 13 chars above or below
+ * @s: the string
+ * Return: the encrypted string
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\n'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
+

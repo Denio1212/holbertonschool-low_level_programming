ab4d06da2bc75b395465e8b924b28193a498f3ae
Denio1212 <shehuden22@yahoo.com>
2022-10-27 05:07:38 -0700

    ALoocator

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory space
+ * @b: the umber of bytes
+ * Return: The number allocated
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+	
+	return (p);
+}
+

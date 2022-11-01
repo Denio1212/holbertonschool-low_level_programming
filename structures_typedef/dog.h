3d770651d216f15ab668a4ad34e26d2249c85850
Denio1212 <shehuden22@yahoo.com>
2022-11-01 06:01:48 -0700

    DOGE

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,10 +1,15 @@
 #ifndef _DOG_H_
 #define _DOG_H_
-
+/**
+* struct dog - holds the info about DOGE
+* @name: the dogs name
+* @age: the age of DOGEE
+* @owner: the lucky bastard
+*/
 typedef struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+char *name;
+float age;
+char *owner;
 } dog;
 #endif

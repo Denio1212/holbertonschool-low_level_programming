a248ab8384c30f403da14f0f4c509569973a604c
Denio1212 <shehuden22@yahoo.com>
2022-11-02 10:37:55 -0700

    This is my identity theft arc

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints the program name with a newline slapped at the end
+ * @argc: stores the argument count
+ * @argv: the strings of the arguments
+ * Return: 0 if true
+ */
+int main(int __attribute__ ((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

a2d186fc081c01f99d18d66dfbb4e1c01c40e8d5
Denio1212 <shehuden22@yahoo.com>
2022-11-02 11:32:38 -0700

    Addiction to addition isnt up to addmission

---
+++ argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - adds only two numbers
+ * @argc: number of arguments
+ * @argv: strings of arguments
+ * Return: 1 if error, 0 if success
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+	char *s;
+
+	if (argc == 1)
+		printf("0\n");
+	else
+		for (i = 0; i < argc; i++)
+		{
+			sum += strtol(argv[i], &s, 0);
+
+			if ((*s >= 'A' && *s <= 'Z') ||
+				(*s >= 'a' && *s <= 'z'))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	printf("%d\n", sum);
+	return (0);
+}

92654df32b60dd59b2c5c0c20fc360f0c048759a
Denio1212 <shehuden22@yahoo.com>
2022-11-02 10:43:22 -0700

    The NUMBERS MASON!

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints out the number of arguments slapped by a new line at the back
+ * @argc: the nr of arguments
+ * @argv: the string contained in it
+ * Return: 0 success
+ */
+int main(int argc, char __attribute__ ((unused)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}

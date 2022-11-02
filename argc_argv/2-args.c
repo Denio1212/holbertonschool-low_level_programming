a3a41aa623b88e01d49706751f20cd9fb8b942df
Denio1212 <shehuden22@yahoo.com>
2022-11-02 10:52:09 -0700

    All of them

---
+++ argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints all arguments recieved
+ * @argc: the count of arguments
+ * @*argv - the strings of each argument
+ * Return: 0 if success
+ */
+int main(int argc, char *argv[])
+{
+	int length;
+
+	if (argc > 1)
+	for (length = 1; length < argc; length++)
+	{
+		printf("%s\n", argv[length]);
+	}
+	return (0);
+}

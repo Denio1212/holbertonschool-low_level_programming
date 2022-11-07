e96b569a0a490d6680e72c2ec16621d968a366db
Denio1212 <shehuden22@yahoo.com>
2022-11-07 07:52:45 -0800

    Comrade Bob

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,6 +1,6 @@
 #include "function_pointers.h"
 /**
- * prinf_name - prints out Bob Dylan
+ * print_name - prints out Bob Dylan
  * @name: Holds the name
  * @f: pointer to the printing function
  */
@@ -11,9 +11,3 @@ void print_name(char *name, void (*f)(char *))
 		f(name);
 	}
 }
-/**
- * what this function does is simply print an imputted name
- * the name is given as char * and f is a function pointer of print_name
- * we firstly make sure that name and the value in f are the same
- * afterwards we simply run the print name function on the given name
- */

4f9b0d5afbb4dd297633296e60c1af2ec0cd68d0
Denio1212 <shehuden22@yahoo.com>
2022-11-09 08:16:09 -0800

    Function holder

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,5 @@
+int op_add(int a, int b);/*adds a and b */
+int op_sub(int a, int b);/*subtracts a and b */
+int op_mul(int a, int b);/*multiplies a and b */
+int op_div(int a, int b);/*divides a with b */
+int op_mod(int a, int b);/*returns the remainder of the division of a and b */

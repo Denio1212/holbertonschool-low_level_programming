5912bd108e0e29b4b55cd3de5ee6a6a763b3fcfd
Denio1212 <shehuden22@yahoo.com>
2022-11-09 10:10:36 -0800

    fff

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -8,5 +8,15 @@ int sum_them_all(const unsigned int n, ...);
 int _putchar(char c);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void print_char(va_list ap);
+void print_int(va_list ap);
+void print_float(va_list ap);
+void print_str(va_list ap);
 
+typedef struct stringus
+{
+	char *str;
+	void (*print)(va_list ap);
+} print_format;
 #endif

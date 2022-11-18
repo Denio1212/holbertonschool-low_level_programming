bb8cabd9565a564c37ecc5503b6022227882a759
Denio1212 <shehuden22@yahoo.com>
2022-11-18 06:21:50 -0800

    lengthy proccess

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,5 +1,9 @@
 #include "lists.h"
-
+/**
+ * list_len - gives the length of a list
+ * @h: the pointer to the next node
+ * Return: how many nodes there are
+ */
 
 size_t list_len(const list_t *h)
 {

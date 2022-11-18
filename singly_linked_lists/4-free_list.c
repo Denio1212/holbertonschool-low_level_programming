f3a716e1047c5828fb53a25fa8d70802a80f5c8e
Denio1212 <shehuden22@yahoo.com>
2022-11-18 07:26:02 -0800

    freer

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,9 +1,12 @@
 #include "lists.h"
 
-
+/**
+ * free_list - frees a node
+ * @head: the pointer to node
+ */
 void free_list(list_t *head)
 {
-	while(head)
+	while (head)
 	{
 		free(head->str);
 		free(head);

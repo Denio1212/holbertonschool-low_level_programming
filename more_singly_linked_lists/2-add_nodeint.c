98173a31e5e78eef5e0cba9392fd59b88ca9aa74
Denio1212 <shehuden22@yahoo.com>
2022-11-18 08:41:04 -0800

    Le head

---
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+
+/**
+ * add_nodeint - makes a node at the beginning
+ * @head: get head
+ * @n: number
+ * Return: head after use
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new-> next = *head;
+	*head = new;
+
+	return (*head);
+}

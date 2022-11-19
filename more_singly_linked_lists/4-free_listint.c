3c7e3644e4a0bbf2fb6adb0801c41071f108957a
Denio1212 <shehuden22@yahoo.com>
2022-11-19 12:46:50 -0800

    Freedom

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a node
+ * @head: the pointer to node
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *replace;
+	while (head)
+	{
+		replace = head->next;
+		free(head);
+		head = replace;
+	}
+}

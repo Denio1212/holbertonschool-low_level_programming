2137cd12d19e7817269bd3fb93e4cb92be8f4fe0
Denio1212 <shehuden22@yahoo.com>
2022-11-22 06:00:56 -0800

    Copy and paste, that easy HUH?

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -12,11 +12,14 @@ int main(int argc, char *argv[])
 	char buffer[1024];
 
 	if (argc != 3)
+	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 	fd1 = open(argv[1], O_RDONLY);
 	if (fd1 < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n");
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
@@ -24,7 +27,7 @@ int main(int argc, char *argv[])
 	{
 		if ((write(fd2, buffer, n)) != n || fd2 < 0)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE\n");
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
@@ -34,15 +37,11 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	if (close(fd1))
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE\n");
-		exit(98);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", exit(100));
+
 	if (close(fd2))
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE\n");
-		exit(98);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", exit(100));
+	
 	return (0);
 }
 

486afb04eaa5e816b37e4570b1c32b413525bdc6
Denio1212 <shehuden22@yahoo.com>
2022-11-22 05:29:44 -0800

    Ending to a tragic tale

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -28,7 +28,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_RDWR | O_APPEND, 0334);
 
 	if (!text_content)
-		return (-1);
+		return (1);
 	if (write(fd, text_content, _strlen(text_content)) == -1)
 		return (-1);
 	close(fd);

744b7814e84b2bb520ba52b1d79bb90a9cf7804a
Denio1212 <shehuden22@yahoo.com>
2022-11-24 08:49:06 -0800

    Wherever you want sire!

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -36,7 +36,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0;
 
 	current = *h;
-	if (!current && idx || idx > dlist_size(*h))
+	if ((!current && idx) || idx > dlist_size(*h))
 		return (NULL);
 	while (i < idx)
 	{

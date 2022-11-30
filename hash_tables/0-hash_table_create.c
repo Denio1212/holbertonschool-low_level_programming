138087a8a83e634177a5d407487bb73879084cf6
Denio1212 <shehuden22@yahoo.com>
2022-11-30 05:35:44 -0800

    make a hash table

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+/**
+* hash_table_create - this will create a hash table
+* @ size: size of hash table
+* Return: pointer to table or NULL
+*/
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+
+	if (!size)
+		return (NULL);
+	table = malloc(sizeof(hash_table_t));
+	if (!table)
+		return (NULL);
+
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *));
+
+	if (!table->array)
+	{
+		free(table);
+		return (NULL);
+	}
+	return (table);
+}
+
+		
+
+	
+

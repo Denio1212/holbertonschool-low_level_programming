11d38c9571b0d37e3e6e0db07bc1f5140a32c51b
Denio1212 <shehuden22@yahoo.com>
2023-01-04 02:46:16 -0800

    Head

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,23 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+* Funcs
+* Task 0 
+*/
+int linear_search(int *array, size_t size, int value);
+
+/* Task 1 */
+
+int binary_search(int *array, size_t size, int value);
+
+
+
+
+
+
+#endif
